2777f48052e3895cc2d059cf04ae64af60fd8122
attilakuti <attilakuti94@gmail.com>
2020-09-28 23:20:21 +0100

    Added the QuestionSeeder class to the run/call method of the DatabaseSeeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(QuestionnaireSeeder::class);
+        $this->call([
+            QuestionnaireSeeder::class,
+            QuestionSeeder::class, 
+        ]);
     }
 }
