d214ad2426e374386acce1340c092c39b858bb68
attilakuti <attilakuti94@gmail.com>
2020-09-28 23:12:05 +0100

    Added 12 question seeds to the QuestionSeeder class

--- database/seeds/QuestionSeeder.php
+++ database/seeds/QuestionSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class QuestionSeeder extends Seeder
 {
@@ -11,6 +13,19 @@ class QuestionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('questions')->insert([
+            ['questionnaire_id' => 1, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 1],
+            ['questionnaire_id' => 1, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 2],
+            ['questionnaire_id' => 1, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 3],
+            ['questionnaire_id' => 1, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 4],
+            ['questionnaire_id' => 2, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 5],
+            ['questionnaire_id' => 2, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 6],
+            ['questionnaire_id' => 2, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 7],
+            ['questionnaire_id' => 2, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 8],
+            ['questionnaire_id' => 3, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 9],
+            ['questionnaire_id' => 3, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 10],
+            ['questionnaire_id' => 3, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 11],
+            ['questionnaire_id' => 3, 'question' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit?", 'order-key' => 12],
+        ]);
     }
 }
