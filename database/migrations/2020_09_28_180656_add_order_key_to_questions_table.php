273ac89a5e8595f29e4f6e8968ca2e62ee4fc8f5
attilakuti <attilakuti94@gmail.com>
2020-09-28 19:14:50 +0100

    Added the numerical auto_increment order_key field

--- database/migrations/2020_09_28_180656_add_order_key_to_questions_table.php
+++ database/migrations/2020_09_28_180656_add_order_key_to_questions_table.php
@@ -14,7 +14,7 @@ class AddOrderKeyToQuestionsTable extends Migration
     public function up()
     {
         Schema::table('questions', function (Blueprint $table) {
-            //
+            $table->increments('order_key');
         });
     }
 
