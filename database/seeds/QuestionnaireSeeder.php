da2f7f85102a6e873d1a64b554b8a8881543dd2b
attilakuti <attilakuti94@gmail.com>
2020-09-28 23:56:52 +0100

    Added slugs to the QuestionnaireSeeder

--- database/seeds/QuestionnaireSeeder.php
+++ database/seeds/QuestionnaireSeeder.php
@@ -14,9 +14,9 @@ class QuestionnaireSeeder extends Seeder
     public function run()
     {
         DB::table('questionnaires')->insert([
-            ['name' => "Questionnaire 1"],
-            ['name' => "Questionnaire 2"],
-            ['name' => "Questionnaire 3"]
+            ['name' => "Questionnaire 1", 'slug' => "questionnaire-1"],
+            ['name' => "Questionnaire 2", 'slug' => "questionnaire-2"],
+            ['name' => "Questionnaire 3", 'slug' => "questionnaire-3"]
         ]);
     }
 }
