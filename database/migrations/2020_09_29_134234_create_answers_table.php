5f4e4ff9b0fc93940cb6976e6268778a33da2282
attilakuti <attilakuti94@gmail.com>
2020-09-29 23:21:24 +0100

    Created a migration for the answers table

---
+++ database/migrations/2020_09_29_134234_create_answers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('questionnaire_id');
+            $table->string('email');
+            $table->string('first_answer');
+            $table->string('second_answer');
+            $table->string('third_answer');
+            $table->string('fourth_answer');
+            $table->timestamps();
+            $table->foreign('questionnaire_id')->references('id')->on('questionnaires');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
